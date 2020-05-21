e338a7085f94778db92eaa1cf97ff7f7ff7504a6
Michał 'Khorne' Lowas-Rzechonek <michal@rzechonek.net>
2020-05-21 12:14:30 +0200

    v0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 # fmt: off
 setup(
     name='pytest-thawgun',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
     url='https://github.com/mrzechonek/thawgun',
     license='Apache 2.0',
